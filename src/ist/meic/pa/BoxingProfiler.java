c006289908c3df23b3b826a5ee268cab83b0c1b6
Rui Silva <ruimams@gmail.com>
2016-03-13 22:52:46 +0000

    Set basic project structure

---
+++ src/ist/meic/pa/BoxingProfiler.java
@@ -0,0 +1,9 @@
+package ist.meic.ap;
+
+import javassist.*;
+
+public class BoxingProfiler {
+    public static void main(String[] args) {
+        ClassPool pool = ClassPool.getDefault();
+    }
+}
