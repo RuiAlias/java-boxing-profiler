094734614890e43e222d97072c141b441a9ea1a1
Rui Silva <ruimams@gmail.com>
2016-03-16 01:43:37 +0000

    Implement responsability transfer to translator

--- src/main/ist/meic/ap/BoxingProfiler.java
+++ src/main/ist/meic/ap/BoxingProfiler.java
@@ -3,7 +3,18 @@ package ist.meic.ap;
 import javassist.*;
 
 public class BoxingProfiler {
-    public static void main(String[] args) {
-        ClassPool pool = ClassPool.getDefault();
+  public static void main(String[] args)
+      throws NotFoundException, CannotCompileException, Throwable {
+    if (args.length < 1) {
+      System.out.println("Error: Argument missing");
+    } else {
+      Translator translator = new BoxingProfilerTranslator();
+      ClassPool pool = ClassPool.getDefault();
+      Loader classLoader = new Loader();
+      classLoader.addTranslator(pool, translator);
+      String[] restArgs = new String[args.length - 1];
+      System.arraycopy(args, 1, restArgs, 0, restArgs.length);
+      classLoader.run(args[0], restArgs);
     }
+  }
 }
---
+++ src/main/ist/meic/ap/BoxingProfilerTranslator.java
@@ -0,0 +1,19 @@
+package ist.meic.ap;
+
+import javassist.*;
+
+public class BoxingProfilerTranslator implements Translator {
+  public void start(ClassPool pool)
+      throws NotFoundException, CannotCompileException {
+  }
+
+  public void onLoad(ClassPool pool, String className)
+      throws NotFoundException, CannotCompileException {
+    CtClass ctClass = pool.get(className);
+    makeBoxingProfiler(ctClass);
+  }
+
+  void makeBoxingProfiler(CtClass ctClass)
+      throws NotFoundException, CannotCompileException {
+  }
+}
