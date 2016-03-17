4ccae4fe71a16425276ccd551dde27caddf4b370
Rui Silva <ruimams@gmail.com>
2016-03-17 13:50:02 +0000

    Remove unnecessary prints

--- src/main/ist/meic/ap/ProfilingResults.java
+++ src/main/ist/meic/ap/ProfilingResults.java
@@ -8,21 +8,14 @@ public class ProfilingResults {
   static SortedMap<String, Integer> sortedResults = new TreeMap<String, Integer>();
 
   public static void add(String key) {
-    System.out.println("Entering add ------------");
-
     if (!sortedResults.containsKey(key)) {
       sortedResults.put(key, 0);
-      System.out.println("Doesnt contain key");
     }
 
     sortedResults.put(key, sortedResults.get(key) + 1);
-    System.out.println("asdasd " + sortedResults.size());
   }
 
   public static void printSortedResults() {
-    System.out.println("Printing Sorted Results");
-    System.out.println("Sorted Results size " + sortedResults.size());
-
     for (Map.Entry<String, Integer> entry : sortedResults.entrySet()) {
       String[] words = entry.getKey().split(" ");
       System.out.println(words[0] + " " + words[2] + " " + entry.getValue() + " " + words[1]);
