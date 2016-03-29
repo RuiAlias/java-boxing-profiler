22c2de16ef6b8891a0e29f2b40bb089752e82a2a
Rui Silva <ruimams@gmail.com>
2016-03-29 16:44:37 +0100

    Remove unnecessary commented code

--- src/main/ist/meic/pa/ProfilingResults.java
+++ src/main/ist/meic/pa/ProfilingResults.java
@@ -10,17 +10,6 @@ import java.util.Map;
  */
 public class ProfilingResults {
   static SortedMap<String, Integer> sortedResults = new TreeMap<String, Integer>();
-
-  /*
-  static long totalTime = 0;
-  
-  public static void addTime(long t){
-	  totalTime += t;
-  }
-  
-  public static long getTotalTime(){return totalTime;}
-  */
-  
   public static void add(String key) {
     if (!sortedResults.containsKey(key)) {
       sortedResults.put(key, 0);
